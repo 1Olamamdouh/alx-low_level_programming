7f6241cdf7d66299be875b657ee915174aa08083
1Olamamdouh <mamdouhola7@>
2023-04-08 16:21:30 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,11 +5,11 @@
  * @argc: count of the arguments supplied to the program and the
  * @argv: an array of pointers to the strings
  * Return: Always 0
- */
+*/
 
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
+	if (argc > 0)
+		printf("%s\n", argv[0]);
 	return (0);
 }
