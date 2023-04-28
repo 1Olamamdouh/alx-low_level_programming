7283deeb68f2c15831dff35b4e3258c10afdefc6
1Olamamdouh <mamdouhola7@>
2023-04-28 03:53:25 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,16 +1,21 @@
 #include "lists.h"
-/**
-  * listint_len - gets the number of nodes in a list
-  *
-  * @h: singly linked list to print
-  *
-  * Return: number of nodes in the list
-  */
-size_t listint_len(const listint_t *h)
-{
-	size_t i;
 
-	for (i = 0; h; i++)
+/**
+ * list_len - Entry point
+ *
+ * @h: pointer
+ *
+ * Return: size of list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
 		h = h->next;
+		i++;
+	}
 	return (i);
 }
